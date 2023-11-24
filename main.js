4d2abc485269f386f28f8ff2f44a8110bbe06b24
sifutcon <138475593+sifutcon@users.noreply.github.com>
2023-11-24 08:58:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "gpt-3.5-turbo-instruct",
+        model: "gpt-3.5-turbo-16k",
         prompt: prompt,
-        max_tokens: 1024,
+        max_tokens: 10000,
         n: 1,
         temperature: 0.1,
       }),
