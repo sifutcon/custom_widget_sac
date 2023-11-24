aaa72da12e578dba4a7e716228a40af434365554
sifutcon <138475593+sifutcon@users.noreply.github.com>
2023-11-24 13:43:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
-const openai = new OpenAI({
-});
+const openai = new OpenAI(
+);
 var ajaxCall = (key, url, prompt) => {
   return new Promise((resolve, reject) => {
     $.ajax({
